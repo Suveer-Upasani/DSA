05ef867df5ce58e0eaac8fcef0a05991e2a2e191
Suveer-Upasani <suveerupasani@gmail.com>
2025-09-14 19:08:37 +0530

    Update bubble_sort.cpp

--- sorting/bubble_sort.cpp
+++ sorting/bubble_sort.cpp
@@ -1,33 +1,30 @@
 #include <iostream>
 using namespace std;
-void Bubble(int a[], int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (a[i] <= a[j])
-            {
-                swap(a[i], a[j]);
+
+void Bubble(int a[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                swap(a[j], a[j + 1]);
             }
         }
     }
 }
-int main()
-{
+
+int main() {
     int n;
-    cout << "Enter Size of Array:";
+    cout << "Enter Size of Array: ";
     cin >> n;
     int *a = new int[n];
-    cout << "Enter Elements of Array:";
-    for (int i = 0; i < n; i++)
-    {
+    cout << "Enter Elements of Array: ";
+    for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
     Bubble(a, n);
-    cout << "Sorted Array is:";
-    for (int i = 0; i < n; i++)
-    {
+    cout << "Sorted Array is: ";
+    for (int i = 0; i < n; i++) {
         cout << a[i] << " ";
     }
+    delete[] a;
+    return 0;
 }
